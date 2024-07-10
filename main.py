c2d3c18ab2ae63146ae4ffab0ce6d119a0256bd6
adore <perekrutq@gmail.com>
2024-07-11 01:28:43 +0500

    add downloads and minor changes

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from config import API_TOKEN
 from PyPDF2 import PdfReader
 
-# API_TOKEN = '7168496497:AAENOQkMy0mYSVdPBRzvk_WzJaggxyOc-jY'
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 users_books = {}
+books = {}
 
 def save_data(data, filename):
     with open(filename, 'w', encoding='utf-8') as f:
@@ -30,6 +30,8 @@ def load_data(filename):
         return {}
 
 users_books = load_data('users_books.json')
+users_books = {int(k): v for k, v in users_books.items()}
+books = load_data('books.json')
 
 def extract_text_from_pdf(pdf_file):
     pdf = PdfReader(pdf_file)
@@ -45,61 +47,97 @@ def split_text_into_pages(text):
     return pages
 
 
-def get_navigation_keyboard(page_index, total_pages):
+def get_navigation_keyboard(page_index, total_pages, filename):
     builder = InlineKeyboardBuilder()
     if page_index > 0:
-        builder.add(InlineKeyboardButton(text='⬅️ Назад', callback_data=f'prev_{page_index}'))
+        builder.add(InlineKeyboardButton(text='⬅️', callback_data=f'prev_{filename}_{page_index}'))
     if page_index < total_pages - 1:
-        builder.add(InlineKeyboardButton(text='Вперед ➡️', callback_data=f'next_{page_index}'))
-    builder.add(InlineKeyboardButton(text=f"{page_index + 1}/{total_pages}", callback_data="page_info", disabled=True))
+        builder.add(InlineKeyboardButton(text='➡️', callback_data=f'next_{filename}_{page_index}'))
+    builder.add(InlineKeyboardButton(text=f'{page_index + 1}/{total_pages}', callback_data='page_info', disabled=True))
+    builder.add(InlineKeyboardButton(text='Файлы', callback_data='downloads'))
     return builder.as_markup()
 
-def get_library_keyboard(chat_id):
+
+def get_library_keyboard(user_id):
     builder = InlineKeyboardBuilder()
     i = 0
-    for book in users_books[chat_id]:
-        builder.add(InlineKeyboardButton(text=book, callback_data=f'book_{i}'))
+    for book in users_books[user_id]:
+        builder.add(InlineKeyboardButton(text=book.split('.')[0], callback_data=f'book_{book}')) 
         i += 1
+    return builder.as_markup()
+
+
+def get_start_keyboard(user_id):
+    builder = InlineKeyboardBuilder()
+    # builder.add(InlineKeyboardButton(text='Загрузить файл', callback_data='load_file'))
+    try:
+        if users_books[user_id] is not None:
+            builder.add(InlineKeyboardButton(text='Загруженные файлы', callback_data='downloads'))
+    except:
+        pass
     return builder.as_markup()
 
 
 @dp.message(Command('start'))
 async def start_command(message: types.Message):
-    chat_id = message.chat.id
-    if chat_id in users_books:
-        await message.answer('Список загруженных книг: ', reply_markup=get_library_keyboard())
-    else:
-        await message.answer('Привет! Здесь ты можешь удобно читать книги.')
+    user_id = message.chat.id
+    await message.answer('Привет! Здесь ты можешь удобно читать книги.', reply_markup=get_start_keyboard(user_id))
+
+
+@dp.message(Command('downloads'))
+async def start_command(message: types.Message):
+    user_id = message.chat.id
+    await message.answer('Загруженные файлы:', reply_markup=get_library_keyboard(user_id))
 
 
 @dp.message(F.document.mime_type == 'application/pdf')
 async def handle_new_pdf(message: types.Message):
-    chat_id = message.chat.id
-    if chat_id in users_books:
-        if message.document.file_name in users_books[chat_id]:
-            await message.answer('Книга уже скачана.')
-            await message.answer('Список загруженных книг: ', reply_markup=get_library_keyboard())
+    user_id = message.chat.id
+    filename = message.document.file_name
+
+    if user_id in users_books and filename in users_books[user_id]:
+        await message.answer('Книга уже скачана. Список загруженных книг: ', reply_markup=get_library_keyboard(user_id))
     else:
         pdf_file = io.BytesIO()
         await bot.download(file=message.document, destination=pdf_file)
         await message.answer('Идёт загрузка файла...')
-        try:
-            text = extract_text_from_pdf(pdf_file)
-            pages = split_text_into_pages(text)
-            users_books[chat_id] = pages
-            await message.answer(pages[0], reply_markup=get_navigation_keyboard(0, len(pages)))
-            save_data(users_books, 'users_books.json') 
-        except Exception as e:
-            await message.reply('Произошла ошибка при обработке PDF файла.')
-            logging.error(f'Error processing PDF: {e}')
+
+        text = extract_text_from_pdf(pdf_file)
+        pages = split_text_into_pages(text)
+        if user_id not in users_books:
+            users_books[user_id] = []
+        users_books[user_id].append(filename)
+        books[filename] = pages
+
+        await message.answer(pages[0], reply_markup=get_navigation_keyboard(0, len(pages), filename))
+        save_data(users_books, 'users_books.json') 
+        save_data(books, 'books.json')
 
 
+@dp.callback_query(F.data == 'downloads')
+async def downloads(callback_query: types.CallbackQuery):
+    user_id = callback_query.message.chat.id
+    await bot.edit_message_text('Загруженные файлы:', chat_id=user_id, message_id=callback_query.message.message_id,
+                                reply_markup=get_library_keyboard(user_id))
+
+
+@dp.callback_query(F.data.startswith('book_'))
+async def downloads(callback_query: types.CallbackQuery):
+    user_id = callback_query.message.chat.id
+    data = callback_query.data.split('_')
+    filename = data[1]
+    pages = books[filename]
+    await bot.edit_message_text(pages[0], chat_id=user_id, message_id=callback_query.message.message_id,
+                                reply_markup=get_navigation_keyboard(0, len(pages), filename))
+
+
+# turn page
 @dp.callback_query(F.data.startswith(('prev_', 'next_')))
 async def process_navigation(callback_query: types.CallbackQuery):
-    chat_id = callback_query.message.chat.id
-    
+    user_id = callback_query.message.chat.id
+
     data = callback_query.data.split('_')
-    action, page_index = data[0], int(data[1])
+    action, filename, page_index = data[0], data[1], int(data[2])
     
     if action == 'prev':
         new_index = page_index - 1
@@ -108,30 +146,11 @@ async def process_navigation(callback_query: types.CallbackQuery):
     else:
         return
     
-    pages = users_books[chat_id]
+    pages = books[filename]
     text = pages[new_index]
-    await bot.edit_message_text(text, chat_id=chat_id, message_id=callback_query.message.message_id,
-                                reply_markup=get_navigation_keyboard(new_index, len(pages)))
-    await callback_query.answer()
-
-
-@dp.message()
-async def handle_page_number(message: types.Message):
-    chat_id = message.chat.id
-    try:
-        page_number = int(message.text)
-        if chat_id in users_books:
-            pages = users_books[chat_id]
-            total_pages = len(pages)
-            if 1 <= page_number <= total_pages:
-                await bot.delete_message(chat_id=chat_id, message_id=message.message_id - 1)
-                await message.answer(pages[page_number - 1], reply_markup=get_navigation_keyboard(page_number - 1, total_pages))
-            else:
-                await message.answer(f"Пожалуйста, введите номер страницы от 1 до {total_pages}.")
-        else:
-            await message.answer("Пожалуйста, отправьте PDF файл.")
-    except ValueError:
-        await message.answer("Пожалуйста, введите корректный номер страницы.")
+    await bot.edit_message_text(text, chat_id=user_id, message_id=callback_query.message.message_id,
+                                reply_markup=get_navigation_keyboard(new_index, len(pages), filename))
+    # await callback_query.answer()
 
 
 async def main():
